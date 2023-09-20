bb70d65787626d10c29e19a2c0fb796540176b24
Piotr B <piotr.bobel23@gmail.com>
2023-09-20 19:23:41 +0200

    site footer component added

--- bootstrap-app/src/app/components/main-site/main-site.component.ts
+++ bootstrap-app/src/app/components/main-site/main-site.component.ts
@@ -7,18 +7,20 @@ import { CommonModule } from '@angular/common';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { SiteFooterComponent } from './site-footer/site-footer.component';
 
 @Component({
   selector: 'app-main-site',
   standalone: true,
+  templateUrl: './main-site.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     TopNavbarComponent,
     SideNavbarComponent,
     GalleryComponent,
+    SiteFooterComponent,
   ],
-  templateUrl: './main-site.component.html',
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainSiteComponent {}
